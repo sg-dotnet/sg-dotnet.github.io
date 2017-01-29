e01903f97cd39b46fbbb5fa1cf0a68bf900e94b9
goh-chunlin <chunlin@live.com.sg>
2017-01-29 23:03:24 +0800

    Removed unused IHostingEnvironment env.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,8 +4,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CommunityWeb.Models;
 using Microsoft.Extensions.Options;
-using Microsoft.AspNetCore.Hosting;
-using System.IO;
 using CommunityWeb.Util;
 using Microsoft.Azure.Documents.Client;
 using System.Collections.Generic;
@@ -16,16 +14,10 @@ namespace CommunityWeb.Controllers
     public class HomeController : Controller
     {
         private readonly AppSettings _appSettings;
-        private readonly string _pathToJsonDataFile;
 
-        public HomeController(IOptions<AppSettings> appSettings, IHostingEnvironment env)
+        public HomeController(IOptions<AppSettings> appSettings)
         {
             _appSettings = appSettings.Value;
-
-            _pathToJsonDataFile = env.ContentRootPath +
-                Path.DirectorySeparatorChar.ToString() + "wwwroot" +
-                Path.DirectorySeparatorChar.ToString() + "data" +
-                Path.DirectorySeparatorChar.ToString();
         }
 
         public async Task<IActionResult> Index()
